d24575e660a8835ed8ec583fbaa669468acab4e3
koo-rogie <rn5184@naver.com>
2025-02-19 14:18:31 +0900

    23번 스크립트 파일 생성 및 수정 완성

---
+++ src/html/js/23-script.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+  //   선생님꺼
+  document.querySelector('.flip-button').addEventListener('click', () => {
+    document.querySelector('.card').classList.toggle('is-flipped');
+  });
+
+  // 내꺼
+  // DOM 선택 부분
+  //   const flipButton = document.querySelector('.flip-button');
+  //   const card = document.querySelector('.card');
+
+  //   // 함수 구현 부분
+  //   const flipCard = () => {
+  //     card.classList.toggle('is-flipped');
+  //   };
+
+  //   // 이벤트 바인딩 부분
+  //   flipButton.addEventListener('click', flipCard);
+};
